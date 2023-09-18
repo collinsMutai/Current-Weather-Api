ed53dac5edb009932e59141ec1b7731170ff178d
Collins Mutai <collinsfrontend@gmail.com>
2023-09-18 18:36:30 +0300

    initial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
-function App() {
+import info from "./data.json";
+
+const App = () => {
+  const [data, setData] = useState({});
+
+  const searchFunc = (area) => {
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${area}&units=imperial&appid=${info.key}`;
+
+    axios.get(url).then((response) => {
+      setData(response.data);
+      console.log(response.data);
+    });
+  };
+  useEffect(() => {
+    searchFunc("litein");
+  }, []);
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+    searchFunc(event.target.area.value);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="search">
+        <form onSubmit={submitHandler}>
+          <input type="text" name="area" placeholder="Search City" />
+          <button type="submit" className="searchBtn">
+            Search
+          </button>
+        </form>
+      </div>
+      <div className="container">
+        <div className="top">
+          <div className="location">
+            <p>{data.name}</p>
+          </div>
+          <div className="temp">
+            {data.main ? <h1>{Math.round(data.main["temp"])}℉</h1> : null}
+          </div>
+          <div className="description">
+            {data.main ? <p>{data.weather[0]["main"]}</p> : null}
+          </div>
+        </div>
+        {data.name != undefined && (
+          <div className="bottom">
+            <div className="feels">
+              {data.main ? (
+                <p className="bold">{Math.round(data.main["feels_like"])}℉</p>
+              ) : null}
+              <p>Feels Like</p>
+            </div>
+            <div className="humidity">
+              {data.main ? (
+                <p className="bold">{Math.round(data.main["humidity"])}%</p>
+              ) : null}
+              <p>Humidity</p>
+            </div>
+            <div className="wind">
+              {data.main ? (
+                <p className="bold">{Math.round(data.wind["speed"])}MPH</p>
+              ) : null}
+              <p>Wind Speed</p>
+            </div>
+          </div>
+        )}
+      </div>
     </div>
   );
-}
+};
 
 export default App;
