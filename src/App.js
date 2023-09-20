4d549ae44ab7b0ebba290b105fa4d132c32920c9
Collins Mutai <collinsfrontend@gmail.com>
2023-09-20 21:58:41 +0300

    initial

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import axios from "axios";
 import info from "./data.json";
 
 const App = () => {
-  const [data, setData] = useState({});
+  const [data, setData] = useState(info);
 
   const searchFunc = (area) => {
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${area}&units=imperial&appid=${process.env.REACT_APP_API_KEY}`;
@@ -15,7 +15,7 @@ const App = () => {
     });
   };
   useEffect(() => {
-    searchFunc("litein");
+    searchFunc("Litein");
   }, []);
   const submitHandler = (event) => {
     event.preventDefault();
@@ -23,51 +23,53 @@ const App = () => {
     searchFunc(event.target.area.value);
   };
   return (
-    <div className={`app ${data.main["temp"] > 60 ? "sunny" : "cold"}`}>
-      <div className="search">
-        <form onSubmit={submitHandler}>
-          <input type="text" name="area" placeholder="Search City" />
-          <button type="submit" className="searchBtn">
-            Search
-          </button>
-        </form>
-      </div>
-      <div className="container">
-        <div className="top">
-          <div className="location">
-            <p>{data.name}</p>
-          </div>
-          <div className="temp">
-            {data.main ? <h1>{Math.round(data.main["temp"])}℉</h1> : null}
-          </div>
-          <div className="description">
-            {data.main ? <p>{data.weather[0]["main"]}</p> : null}
-          </div>
+    <>
+      <div className={`app ${data.main["temp"] > 60 ? "sunny" : "cold"}`}>
+        <div className="search">
+          <form onSubmit={submitHandler}>
+            <input type="text" name="area" placeholder="Search City" />
+            <button type="submit" className="searchBtn">
+              Search
+            </button>
+          </form>
         </div>
-        {data.name != undefined && (
-          <div className="bottom">
-            <div className="feels">
-              {data.main ? (
-                <p className="bold">{Math.round(data.main["feels_like"])}℉</p>
-              ) : null}
-              <p>Feels Like</p>
+        <div className="container">
+          <div className="top">
+            <div className="location">
+              <p>{data.name}</p>
             </div>
-            <div className="humidity">
-              {data.main ? (
-                <p className="bold">{Math.round(data.main["humidity"])}%</p>
-              ) : null}
-              <p>Humidity</p>
+            <div className="temp">
+              {data.main ? <h1>{Math.round(data.main["temp"])}℉</h1> : null}
             </div>
-            <div className="wind">
-              {data.main ? (
-                <p className="bold">{Math.round(data.wind["speed"])}MPH</p>
-              ) : null}
-              <p>Wind Speed</p>
+            <div className="description">
+              {data.main ? <p>{data.weather[0]["main"]}</p> : null}
             </div>
           </div>
-        )}
+          {data.name != undefined && (
+            <div className="bottom">
+              <div className="feels">
+                {data.main ? (
+                  <p className="bold">{Math.round(data.main["feels_like"])}℉</p>
+                ) : null}
+                <p>Feels Like</p>
+              </div>
+              <div className="humidity">
+                {data.main ? (
+                  <p className="bold">{Math.round(data.main["humidity"])}%</p>
+                ) : null}
+                <p>Humidity</p>
+              </div>
+              <div className="wind">
+                {data.main ? (
+                  <p className="bold">{Math.round(data.wind["speed"])}MPH</p>
+                ) : null}
+                <p>Wind Speed</p>
+              </div>
+            </div>
+          )}
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
